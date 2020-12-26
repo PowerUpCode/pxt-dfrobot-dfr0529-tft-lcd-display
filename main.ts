2ab3dfc48a9ea1d13b9c60e623f6de2f868f97cd
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-12-26 19:14:01 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/**
+ * SEN0304 mudule
+ */
+//% weight=100 color=#DF6721 icon="\uf013" block="LCD Display"
 namespace LCD0529 {
+
+    export function setup(SPI_MOSI: number , SPI_SCL: number, WR: number, CS: number, RS: number, LCK: number ) {
+
+    }
 
 }
