7445b572fe5b3dd2bb5929065544ad0dc5bda188
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-12-26 19:32:38 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -7,13 +7,13 @@
  */
 //% weight=10 color=#DF6721 icon="\uf013" block="LCD Display"
 namespace LCD0529 {
+
     /**
 	 * LCD Initialize
 	*/
     //% blockId=LCD_Init block="Init LCD SPI_MOSI|%SPI_MOSI|SPI_SCL|%SPI_SCL|WR|%WR|CS|%CS|RS|%RS|LCK|%LCK"
-    //% weight=100
-
-    export function setup(SPI_MOSI: number , SPI_SCL: number, WR: number, CS: number, RS: number, LCK: number ): void  {
+    //% weight=200
+    export function setup(SPI_MOSI: DigitalPin , SPI_SCL: DigitalPin, WR: DigitalPin, CS: DigitalPin, RS: DigitalPin, LCK: DigitalPin ): void  {
 
     }
 
