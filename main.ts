235d612116878679d270ff45ea52817bef85e376
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-12-26 19:26:25 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+/**
+ * makecode PowerUpCode DFRobot SPI LCD display package.
+ */
+
+/**
+ * LCD0529 mudule
+ */
+//% weight=10 color=#DF6721 icon="\uf013" block="LCD Display"
 namespace LCD0529 {
     /**
 	 * LCD Initialize
 	*/
-    //% blockId=LCD_Init block="SPI_MOSI|%SPI_MOSI|SPI_SCL|%SPI_SCL|WR|%WR|CS|%CS|RS|%RS|LCK|%LCK"
+    //% blockId=LCD_Init block="Init LCD SPI_MOSI|%SPI_MOSI|SPI_SCL|%SPI_SCL|WR|%WR|CS|%CS|RS|%RS|LCK|%LCK"
     //% weight=100
 
     export function setup(SPI_MOSI: number , SPI_SCL: number, WR: number, CS: number, RS: number, LCK: number ): void  {
