19ed6bae00afb8067d616fda847daf073626e368
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-12-27 18:49:04 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@
  * makecode PowerUpCode DFRobot SPI LCD display package.
  */
 
+
+let CS: DigitalPin
+let RS: DigitalPin
+let WR: DigitalPin
+let LCK: DigitalPin
+
 /**
  * LCD0529 mudule
  */
@@ -20,15 +26,27 @@ namespace LCD0529 {
      * 
      * 
 	*/
-    //% blockId=LCD_Init block="Init LCD SPI_MOSI|%SPI_MOSI|SPI_SCK|%SPI_SCK|WR|%WR|CS|%CS|RS|%RS|LCK|%LCK"
+    //% blockId=LCD_Init block="Init LCD SPI_MOSI|%SPI_MOSI|SPI_SCK|%SPI_SCK|CS_|%CS_|RS_|%RS_|WR_|%WR_|LCK_|%LCK_"
     //% weight=400 blockGap=8
-    export function setup(SPI_MOSI: DigitalPin , SPI_SCK: DigitalPin, WR: DigitalPin, CS: DigitalPin, RS: DigitalPin, LCK: DigitalPin ): void  {
+    export function setup(SPI_MOSI: DigitalPin , SPI_SCK: DigitalPin, CS_: DigitalPin, RS_: DigitalPin, WR_: DigitalPin, LCK_: DigitalPin ): void  {
+        
+        CS = CS_
+        RS = RS_
+        WR = WR_
+        LCK = LCK_
+
+
         pins.spiPins(SPI_MOSI, DigitalPin.P14, SPI_SCK)
+        pins.spiFrequency(4000000)
+
  
         pins.digitalWritePin(CS, 1)
         pins.digitalWritePin(RS, 1)
         pins.digitalWritePin(WR, 1)
         pins.digitalWritePin(LCK, 1)
+
+
+        
     }
 
     //% blockId=Test_kuba block="Czy jest dobrze OK %ok"
@@ -41,6 +59,47 @@ namespace LCD0529 {
 
     function writeCmd(cmd: number): void {
 
+        pins.digitalWritePin(RS, 0)
+        pins.digitalWritePin(CS, 0)
+
+        spiWrite(cmd, true)
+
+        pins.digitalWritePin(WR, 0)
+        pDelay()
+        pins.digitalWritePin(WR, 1)
+        pins.digitalWritePin(CS, 1)
+
+    }
+
+
+    function writeData(dat: number): void {
+
+        pins.digitalWritePin(RS, 1)
+        pins.digitalWritePin(CS, 0)
+
+        spiWrite(dat, true)
+
+        pins.digitalWritePin(WR, 0)
+        pDelay()
+        pins.digitalWritePin(WR, 1)
+        pins.digitalWritePin(CS, 1)        
+
+    }
+
+    function spiWrite(cmd: number, action: boolean) {
+
+        pins.spiWrite(cmd)
+
+        if(action) {
+            pins.digitalWritePin(LCK, 1)
+            pDelay()
+            pins.digitalWritePin(LCK, 0)
+        }
+
+    }
+
+    function pDelay() {
+       // control.waitMicros(1)
     }
 
 }
