e2ed5b9ae2a298cbcc862aec898c98591f197118
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2020-12-27 19:15:57 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -45,7 +45,7 @@ namespace LCD0529 {
         pins.digitalWritePin(WR, 1)
         pins.digitalWritePin(LCK, 1)
 
-
+        initLCD()
         
     }
 
@@ -55,6 +55,122 @@ namespace LCD0529 {
 
     }
 
+
+    function initLCD() {
+
+        console.log('ST7687S begin')
+        control.waitMicros(1200)
+
+        writeCmd(0xd7); /// Disable auto read
+        writeDat(0x9f);
+
+        writeCmd(0xE0); /// EE control in
+        writeDat(0x00);
+        control.waitMicros(100);
+
+
+        writeCmd(0xFA); /// Erase EEPROM data
+        writeDat(0x01);
+        control.waitMicros(200); 
+
+        writeCmd(0xE3); /// Read from EEPROM
+        control.waitMicros(200);
+        writeCmd(0xE1);	
+
+
+        writeCmd(0x28); /// display off
+        writeCmd(0x11);	/// Sleep Out
+        control.waitMicros(300);
+        writeCmd(0xc0); /// Vop setting
+        writeDat(0x17);  //ctrL=0x1b 080416 5PCS 0X1E; 8PCS 0X2A ?????
+        writeDat(0x01);  // base on Module      
+
+        writeCmd(0x25); /// Write Contrast
+        writeDat(0x1E);
+        writeCmd(0xC3); /// Bias selection
+        writeDat(0x03);
+
+        writeCmd(0xC4); /// Booster setting
+        writeDat(0x07);
+
+        writeCmd(0xC5);
+        writeDat(0x01);
+
+        writeCmd(0xCB); /// Vg source control
+        writeDat(0x01); // Vg from 2xVdda
+
+        writeCmd(0xB7);
+        writeDat(0x00);
+
+        writeCmd(0xD0); /// Analog circuit setting
+        writeDat(0x1d);
+        writeCmd(0xB5); /// N-line Setting
+        writeDat(0x89);
+
+        writeCmd(0xBD); ///cross talk compensation setting
+        writeDat(0x02);     
+
+        writeCmd(0xF0);	/// Frame Freq. in Temp range A,B,C and D
+        writeDat(0x07);
+        writeDat(0x0C);
+        writeDat(0x0C);
+        writeDat(0x12);   
+
+
+        writeCmd(0xF4);	///TC setting
+        writeDat(0x33);
+        writeDat(0x33);
+        writeDat(0x33);
+        writeDat(0x00);
+        writeDat(0x33);
+        writeDat(0x66);
+        writeDat(0x66);
+        writeDat(0x66);
+
+        writeCmd(0x20);
+        writeCmd(0x2A);
+        writeDat(0x00);
+        writeDat(0x7F);
+
+        writeCmd(0x2B); /// Row address setting
+        writeDat(0x00); // 0~127
+        writeDat(0x7f);    
+
+        writeCmd(0x3A); /// Interface Pixel Format
+        writeDat(0x05); // 16bits/pixel
+
+        writeCmd(0x36); /// Memory data access control
+        writeDat(0x80); //0xc8
+
+
+        writeCmd(0xB0); ///Display Duty Setting
+        writeDat(0x7F); // Duty = 128 duty
+
+        writeCmd(0x29); /// Display On
+
+        writeCmd(0xF9);	/// Set frame RGB value
+        writeDat(0x00);
+        writeDat(0x02);
+        writeDat(0x04);
+        writeDat(0x06);
+        writeDat(0x08);
+        writeDat(0x0a);
+        writeDat(0x0c);
+        writeDat(0x0e);
+        writeDat(0x10);
+        writeDat(0x12);
+        writeDat(0x14);
+        writeDat(0x16);
+        writeDat(0x18);
+        writeDat(0x1A);
+        writeDat(0x1C);
+        writeDat(0x1E);
+
+        writeCmd(0x29); /// Display On       
+
+
+        
+    }
 
 
     function writeCmd(cmd: number): void {
@@ -72,7 +188,7 @@ namespace LCD0529 {
     }
 
 
-    function writeData(dat: number): void {
+    function writeDat(dat: number): void {
 
         pins.digitalWritePin(RS, 1)
         pins.digitalWritePin(CS, 0)
