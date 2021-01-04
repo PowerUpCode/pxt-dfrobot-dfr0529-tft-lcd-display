0f8e3b48f922966f4cb4f313c476f91cbbfa96a6
PowerUpCode <59996579+PowerUpCode@users.noreply.github.com>
2021-01-04 19:31:13 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,16 @@ let SPI_SCK: DigitalPin
 let height = 127
 let width = 127
 
+    const rbits = hex`
+    008040C020A060E0109050D030B070F0088848C828A868E8189858D838B878F8
+    048444C424A464E4149454D434B474F40C8C4CCC2CAC6CEC1C9C5CDC3CBC7CFC
+    028242C222A262E2129252D232B272F20A8A4ACA2AAA6AEA1A9A5ADA3ABA7AFA
+    068646C626A666E6169656D636B676F60E8E4ECE2EAE6EEE1E9E5EDE3EBE7EFE
+    018141C121A161E1119151D131B171F1098949C929A969E9199959D939B979F9
+    058545C525A565E5159555D535B575F50D8D4DCD2DAD6DED1D9D5DDD3DBD7DFD
+    038343C323A363E3139353D333B373F30B8B4BCB2BAB6BEB1B9B5BDB3BBB7BFB
+    078747C727A767E7179757D737B777F70F8F4FCF2FAF6FEF1F9F5FDF3FBF7FFF`
+
 
 enum color_list {
     
@@ -242,7 +252,7 @@ namespace LCD0529 {
     function spiWrite(cmd: number, action: boolean) {
 
 
-        pins.spiWrite(cmd)
+        pins.spiWrite(rbit(cmd))
 
         if(action) {
             pins.digitalWritePin(LCK, 1)
@@ -326,6 +336,21 @@ namespace LCD0529 {
     function writeToRam():void {
         writeCmd(0x2c);
     }
+
+
+    function rbuffer(b: Buffer): Buffer {
+        let output = pins.createBuffer(b.length);
+        for (let i = 0; i < b.length; i++) {
+            let n = b[i]
+            output[i] = rbit(n)
+        }
+        return output
+    }
+
+
+    function rbit(value: number): number {
+        return rbits[value] || 0x00;
+    }    
 
     function pDelay() {
         control.waitMicros(1)
